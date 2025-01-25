76f905ddda638155673e00cc294576759a0ccc59
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-01-25 17:13:07 +0000

    Add files via upload

--- file.py
+++ file.py
@@ -1,86 +1,45 @@
-import time
-print("\033c\033[43;30m\n")
-class simu:
-    def __init__(self):
-        self.name=[]
-        self.text=[]
-    def load(self,files):
-        f1=open(files,"r")
-        content=f1.read()
-        f1.close()
-        content=content.split("|")
-        for n in content:
-            try:
-                m=n.split("#")
-                
-                m0=m[0]
-                m1=m[1].replace("\\n","\n").replace("\\r","\r")
-                self.name=self.name+[m0]
-                self.text=self.text+[m1]
-            except:
-                pass
-    def save(self,files):
-        content=""
-        l=len(self.name)
-        for n in range(l):
-            if n!=0:
-                content=content+"|"
-            content=content+self.name[n]
-            content=content+"#"+self.text[n]
-        f1=open(files,"w")
-        f1.write(content)
-        f1.close() 
-                
-    def ls(self):
-        for n in self.name:
-            print(n)
-    def cat(self,files):
-        r=0
-        for n in self.name:
-            if n==files:
-                print(self.text[r])
-            r=r+1
-    def news(self,files,text):
-        r=True
-        counter=0
-        l=len(self.name)
-        for n in self.name:
-            if n==files:
-                r=False
-                self.text[counter]=text
-            counter+=1    
-        if r:
-           self.name=self.name+[files]
-           self.text=self.text+[text]
-    def append(self,files,text):
-        r=True
-        counter=0
-        l=len(self.name)
-        for n in self.name:
-            if n==files:
-                r=False
-                self.text[counter]=self.text[counter]+text
-            counter+=1    
-        if r:
-           self.name=self.name+[files]
-           self.text=self.text+[text]
-    def reads(self,files):
-        r=True
-        counter=0
-        l=len(self.name)
-        for n in self.name:
-            if n==files:
-                r=False
-                return self.text[counter]
-            counter+=1    
-        if r:
-             print("error")
-             return None
-         
-s=simu()
-s.load("fs.fs")
-s.append("file5","xxxx")
-s.ls()
-print(s.reads("file5"))
-s.save("fs.fs")
 
+import tkinter as tk
+from PIL import Image, ImageTk
+import time
+
+def move_bitmap():
+    global y_position
+
+    # Atualiza a posição do bitmap
+    if y_position < window_height - bitmap_height:
+        y_position += 5  # Ajusta a velocidade da descida
+        canvas.move(bitmap, 0, 5)
+        root.after(50, move_bitmap)  # Chama a função novamente após 50ms
+
+# Cria a janela principal
+root = tk.Tk()
+root.title("Aterrissagem da Nave")
+root.configure(bg="yellow")
+
+# Define as dimensões da janela
+window_width = 400
+window_height = 600
+root.geometry(f"{window_width}x{window_height}")
+
+# Cria o canvas para exibir o bitmap
+canvas = tk.Canvas(root, width=window_width, height=window_height, bg="yellow", highlightthickness=0)
+canvas.pack()
+
+# Carrega a imagem do bitmap
+image = Image.open("bit.png")
+bitmap_image = ImageTk.PhotoImage(image)
+bitmap_width, bitmap_height = image.size
+
+# Calcula a posição inicial
+x_position = (window_width - bitmap_width) // 2  # Centraliza horizontalmente
+y_position = 0
+
+# Adiciona o bitmap ao canvas
+bitmap = canvas.create_image(x_position, y_position, anchor=tk.NW, image=bitmap_image)
+
+# Inicia o movimento do bitmap
+move_bitmap()
+
+# Executa a aplicação
+root.mainloop()
